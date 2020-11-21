1e302ccd7d52049b6c19cae1d3df67c1c11930b6
dmcclung <dylanmcclung@gmail.com>
2020-11-21 12:27:33 -0600

    Added format function

--- Abs.scala
+++ Abs.scala
@@ -1,15 +1,24 @@
-object MyModule {
+object HigherOrderFunctions {
     def abs(n: Int): Int = {
         if (n < 0) -n
         else n
     }
 
-    private def formatAbs(x: Int) = {
-        val msg = "The absolute value of %d is %d"
-        msg.format(x, abs(x))
+    def factorial(n: Int): Int = {
+        def go(n: Int, acc: Int): Int = 
+            if (n <= 0) acc
+            else go(n - 1, n * acc)
+
+        go(n, 1)
+    }
+
+    private def format(name: String, n: Int, f: Int => Int) = {
+        val msg = "The %s of %d is %d"
+        msg.format(name, n, f(n))
     }
 
     def main(args: Array[String]): Unit = {
-        println(formatAbs(-42))
+        println(format("absolute value", -42, abs))
+        println(format("factorial", -42, factorial))
     }
 }
